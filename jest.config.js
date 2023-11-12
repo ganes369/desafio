af527d8604a45631426559a1c0337a19012cac8c
j.a.s <aras.strong8@gmail.com>
2023-11-12 18:37:26 -0300

    todo

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,5 @@ module.exports = {
     "^.+\\.svg$": "jest-svg-transformer",
     "^.+\\.(css|less|scss)$": "identity-obj-proxy",
   },
-  transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
-  },
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
 };
