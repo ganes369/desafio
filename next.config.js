7c48a49a4dffe6820662f173a90328fd9a755e6a
j.a.s <aras.strong8@gmail.com>
2023-11-12 18:13:33 -0300

    todo

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  compiler: {
-    // ssr and displayName are configured by default
-    styledComponents: true,
-    setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
